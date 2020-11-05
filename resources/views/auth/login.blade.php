1ebd70fc70258c92700d59ec8879b6d8d6efdc87
adhitsimple96 <adhitsimple96@gmail.com>
2020-11-05 18:47:03 +0800

    Pembuatan Layout Login & Register
    Membuat Form Login dan Form Register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,169 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <!-- Another Library -->
+    
+    <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+    <style>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+         @import '~pretty-checkbox/src/pretty-checkbox.scss';
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+         * {
+             margin: 0;
+             padding: 0;
+             font-family: 'Nunito', sans-serif;
+         }
+
+         body {
+             background-color: #fafdfb;
+             font-size: 14px;
+         }
+
+        .form-control:focus {
+            box-shadow: none;
+        }
+        
+        .text-small {
+            font-size: 12px;
+            line-height:20px;
+            
+        }
+
+        .card {
+            
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.03);
+            background-color: #fff;
+            border-radius: 3px;
+            border: none;
+        }
+
+        .card .card-header {
+            
+            border-bottom-color: #f9f9f9;
+            line-height: 30px;
+            align-self: center;
+            width: 100%;
+            min-height: 70px;
+            padding: 15px 25px;
+            display: flex;
+            align-items: center;
+            background-color: transparent;
+            
+        }
+
+        .card.card-primary {
+            margin-top: 25%;
+            border: 3px solid #A9B2F6;
+        }
+
+        .register {
+            margin-bottom: 50px;
+        }
+
+        .form-control {
+        border: 1px solid #0080ff;
+        height: 2.7rem;
+        }
+
+        .form-control {
+            border-radius: 10px;
+        }
+
+        .input-group-text, select.form-control:not([size]):not([multiple]), .form-control:not(.form-control-sm):not(.form-control-lg) {
+            font-size: 12px;
+        }
+
+
+        .login-btn {
+            font-size: 14px;
+        }
+
+        .custom-control-label {
+            line-height: 26px;
+            
+        }
+        .h3 {
+            letter-spacing: 1px;
+        }
+
+        
+
+      
+
+
+    
+    </style>
+
+    <title>Semicolon-Login</title>
+  </head>
+  <body>
+    <div class="container">
+        <div class="row ">
+            <div class=" col-md-6 offset-md-3  col-xl-4 offset-xl-4">
+                <div class="card card-primary ">
+                    <div class="card-header font-weight-bold">
+                       <span class="mx-auto h3 mb-n3">LOGIN</span>
+                    </div>
+                    <div class="card-body">
+                        <form action="#" class="needs-validation" method="post" novalidate >
+                            <div class="form-group">
+                                
+                                <input id="username" type="text" placeholder="Nama Pengguna, No HP , Email" class="form-control mt-1" name="username" required autofocus autocomplete="off">
+                                
                             </div>
-                        </div>
+                            <div class="form-group">
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                <input type="password" placeholder="Kata Sandi" id="password" class="form-control mt-1" required>
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
+                            
+                            <div class="form-group mt-5">
+                                <button type="submit" class="login-btn btn btn-outline-primary btn-lg btn-block">Login</button>
                             </div>
-                        </div>
+                            <div class="mt-5 text-center">Forgot Password ?</div>
+                        </form>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                    </div>
+                </div>
+                <div class="card card-primary p-2  mt-4 register text-muted text-center">
+                   <p class="mb-n1 p-2"> Belum Punya Akun ? <a href="#" >Daftar</a></p>
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  </body>
+</html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,180 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+    <!-- Another Library -->
+    
+    <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+    <style>
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+         @import '~pretty-checkbox/src/pretty-checkbox.scss';
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+         * {
+             margin: 0;
+             padding: 0;
+             font-family: 'Nunito', sans-serif;
+         }
+
+         body {
+             background-color: #fafdfb;
+             font-size: 14px;
+         }
+
+        .form-control:focus {
+            box-shadow: none;
+        }
+        
+        .text-small {
+            font-size: 12px;
+            line-height:20px;
+            
+        }
+
+        .card {
+            
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.03);
+            background-color: #fff;
+            border-radius: 3px;
+            border: none;
+        }
+
+        .card .card-header {
+            
+            border-bottom-color: #f9f9f9;
+            line-height: 30px;
+            align-self: center;
+            width: 100%;
+            min-height: 70px;
+            padding: 15px 25px;
+            display: flex;
+            align-items: center;
+            background-color: transparent;
+            
+        }
+
+        .card.card-primary {
+            margin-top: 20%;
+            border: 3px solid #A9B2F6;
+        }
+
+        .register {
+            margin-bottom: 50px;
+        }
+
+        .form-control {
+            border: 1px solid #0080ff;
+            height: 2.7rem;
+        }
+
+        .form-control {
+            border-radius: 10px;
+        }
+
+        .input-group-text, select.form-control:not([size]):not([multiple]), .form-control:not(.form-control-sm):not(.form-control-lg) {
+            font-size: 12px;
+        }
+
+
+        .login-btn {
+            font-size: 14px;
+        }
+
+        .custom-control-label {
+            line-height: 26px;
+            
+        }
+        .h3 {
+            letter-spacing: 1px;
+        }
+
+        
+
+      
+
+
+    
+    </style>
+
+    <title>Semicolon-Register</title>
+  </head>
+  <body>
+    <div class="container">
+        <div class="row ">
+            <div class=" col-md-6 offset-md-3  col-xl-4 offset-xl-4">
+                <div class="card card-primary ">
+                    <div class="card-header font-weight-bold">
+                       <span class="mx-auto h3 mb-n3">REGISTER</span>
+                       
+                    </div>
+                    <div class="card-body">
+                        <p class="text-center">Buat Akun untuk Melihat Foto dan Video dari Teman Anda</p>
+                        <form action="#" class="needs-validation" method="post" novalidate >
+                            <div class="form-group">
+                                
+                                <input id="text" type="text" placeholder="No HP atau Email" class="form-control mt-1" name="email" required autofocus autocomplete="off">
+                                
                             </div>
-                        </div>
+                             <div class="form-group">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                <input type="text" placeholder="Nama Lengkap" id="namaLengkap" name="namaLengkap" class="form-control mt-1" required>
                             </div>
-                        </div>
+                             <div class="form-group">
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                <input type="text" placeholder="Nama Pengguna" id="namaPengguna" name="namaPengguna" class="form-control mt-1" required>
                             </div>
-                        </div>
+                            <div class="form-group">
 
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <input type="password" placeholder="Kata Sandi" name="password" id="password" class="form-control mt-1" required>
                             </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
+                            
+                            <div class="form-group mt-5">
+                                <button type="submit" class="login-btn btn btn-outline-primary btn-lg btn-block">Register</button>
                             </div>
-                        </div>
-                    </form>
+                            
+                        </form>
+
+                    </div>
+                </div>
+                <div class="card card-primary p-2  mt-4 register text-muted text-center">
+                   <p class="mb-n1 p-2"> Sudah Punya Akun ? <a href="#" >Masuk</a></p>
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  </body>
+</html>
