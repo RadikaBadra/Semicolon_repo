966016072ad45eb6ff0a876fb1d92f8130d39690
Tanaka Junyartha <kakacrash747@gmail.com>
2020-11-11 12:52:44 +0800

    Penambahan halaman profile

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Another Library -->
+    
+    <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
+    
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/profile.css') }}">
+    <link rel="stylesheet" href="{{ asset('font-awesome/css/all.css') }}">
+
+    <title>Semicolon-Login</title>
+  </head>
+  <body>
+    <div class="profile-container">
+        <nav class="section-nav">
+            <div class="button-list">
+                <a href="#"><i class="fas fa-bell"></i></a>
+                <a href="#"><i class="fas fa-envelope"></i></a>
+                <a href="#"><i class="fas fa-home"></i></a>
+                <a href="#"><i class="fas fa-search"></i></a>
+                <a href="#"><i class="fas fa-user"></i></a>
+            </div>
+        </nav>
+        <section class="section-account">
+            <span class="name">JINSAKAI</span>
+            <a href="#"><i class="fas fa-cog"></i></a>
+        </section>
+        <main class="section-main">
+            <div class="profile-section">
+                <span class="logo"><i class="fas fa-user-circle"></i></span>
+                <div class="account-desc">
+                    <div class="count-desc">
+                        <span class="post">0</span>
+                        <span class="followers">0</span>
+                        <span class="following">0</span>
+                    </div>
+                    <div class="label-desc">
+                        <span class="post">POST</span>
+                        <span class="followers">FOLLOWERS</span>
+                        <span class="following">FOLLOWING </span>
+                    </div>
+                </div>
+                <div class="account-bio">
+                    <span class="main-bio">JINSAKAI</span>
+                    <span class="second-bio">bio</span>
+                </div>
+            </div>
+            <a href="#" class="btn btn-outline-secondary">Edit Profile</a>
+            <div class="post-section">
+                <div class="logo"><i class="fas fa-image"></i></div>
+                <span class="desc">You have not post anything</span>
+            </div>
+        </main>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  </body>
+</html>
