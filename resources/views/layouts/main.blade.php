aa7c457f692ce7bb4ff97e4883823e54f153e46b
BagoesPantera <bagoespantera01@gmail.com>
2020-11-12 17:34:01 +0800

    perbaikan error

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -15,11 +15,6 @@
         integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
 
     <title>Comedia - Main</title>
-<<<<<<< HEAD
-
-    <link rel="stylesheet" href="css/main.css">
-    
-=======
     <style>
         .card {
             border-radius: 20px;
@@ -76,18 +71,12 @@
         }
         
     </style>
->>>>>>> 1f0724b5b3ad5478fb0eb6ec68d3278d881d2ca7
 </head>
 
 <body>
     <div class="container-fluid p-0">
-<<<<<<< HEAD
-        <nav class="w-100 navbar navbar-expand-lg bg-light navbar-light ">
-            <a class="navbar-brand" href="/">COMEDIA</a>
-=======
         <nav class="w-100 navbar navbar-expand-lg navbar-light">
             <a class="navbar-brand" href="#">COMEDIA</a>
->>>>>>> 1f0724b5b3ad5478fb0eb6ec68d3278d881d2ca7
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -101,7 +90,7 @@
                         <a class="nav-link fas fa-search mr-3"></a>
                     </li>
                     <li class="nav-item">
-                        <a href ="/profile" class="nav-link fas fa-user mr-3"></a>
+                        <a href ="/user-profile" class="nav-link fas fa-user mr-3"></a>
                     </li>
                     <li class="nav-item">
                         <a class="" href="{{ route('logout') }}"
@@ -117,23 +106,16 @@
                 </ul>
             </div>
         </nav>
-                                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                            
-                                        </div>
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+            
+        </div>
 
 
         <div class="container mt-3">
-<<<<<<< HEAD
-            <h1 class =" font-weight-bold mt-5 name" style="font-size:50px">Hello Jin Sakai</h1>
-            <p style="margin-bottom:40px">HERE RECOMMENDED POST FOR YOU <p>
-            <div class="card post mt-3">
-                <i class="far fa-bookmark fas fa-lg ml-auto px-3 mr-2 mt-4 mb-1"></i>
-=======
             <h2>Hello {{ auth()->user()->username }}</h2>
             <h6>HERE RECOMMENDED POST FOR YOU </h6>
             <div class="card mt-3">
                 <i class="far fa-bookmark fas fa-2x ml-auto px-3 mr-2 mt-4 mb-1"></i>
->>>>>>> 1f0724b5b3ad5478fb0eb6ec68d3278d881d2ca7
                 <div class="card-body">
                     <div class="container-photo float-left">
                         <img src="https://meshle.com/content/uploads/2019/04/GamingLP2.jpg" alt="" width="100%" height="100%" >
@@ -150,16 +132,10 @@
                         <h6>Selamat Ulang Tahun Untuk Radika</h6>
                     </div>
                     <div class="right-area ml-auto">
-<<<<<<< HEAD
-                        <i class="fas fa-comment-alt fas fa-lg mr-5"></i>
-                        <i class="fas fa-heart fas fa-lg mr-5"></i>
-                        <i class="fas fa-share-alt fas fa-lg mr-5"></i>
-=======
                         <i class="fas fa-comment-alt fas fa-2x mr-5"></i>
                         <i class="fas fa-heart fas fa-2x mr-5"></i>
                         <i class="fas fa-share-alt fas fa-2x mr-5"></i>
                         
->>>>>>> 1f0724b5b3ad5478fb0eb6ec68d3278d881d2ca7
                     </div>
                 </div>
                 
